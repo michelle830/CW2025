f06157cc7ed31e512cd99c95fd273b15f616e28e
michelle <efymc14@nottingham.edu.my>
2025-11-23 14:24:56 +0800

    Refactored EventSource.java for COMP2042

--- src/main/java/com/comp2042/EventSource.java
+++ src/main/java/com/comp2042/EventSource.java
@@ -1,3 +1,11 @@
+/**
+ * Indicates the origin of a movement event
+ *
+ * USER - the player pressed a key
+ * THREAD - the automatic game timer triggered the movement
+ *
+ * Refactored for COMP2042 to add clatiry and documentation
+ */
 package com.comp2042;
 
 public enum EventSource {
