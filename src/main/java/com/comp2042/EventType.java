982830a5c7c6c48c8b640706f4a937b9c3f61c37
michelle <efymc14@nottingham.edu.my>
2025-11-23 14:30:49 +0800

    Refactored EventType.java for COMP2042

--- src/main/java/com/comp2042/EventType.java
+++ src/main/java/com/comp2042/EventType.java
@@ -1,3 +1,15 @@
+/**
+ * Represents the type of movement or action requested by the player
+ * or triggered by the game engine
+ *
+ * DOWN - move the active brick downward
+ * LEFT - move the brick left
+ * RIGHT -move the brick right
+ * ROTATE - rotate the brick
+ *
+ * Refactored for COMP2042 to add clarity and documentation
+ */
+
 package com.comp2042;
 
 public enum EventType {
