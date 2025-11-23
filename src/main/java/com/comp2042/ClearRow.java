3e9be3161432835e78677c371bc4c67f09c4cc44
michelle <efymc14@nottingham.edu.my>
2025-11-23 13:49:21 +0800

    Refactored ClearRow.java for COMP2042

--- src/main/java/com/comp2042/ClearRow.java
+++ src/main/java/com/comp2042/ClearRow.java
@@ -1,3 +1,12 @@
+/**
+ * Represents the result of clearing rows after a brick mergre.
+ * Stores both:
+ * - how many rows were removed
+ * - the score bonus earned for those removals
+ *
+ * Refactored for COMP2042 to improve naming, clarity and documentation
+ */
+
 package com.comp2042;
 
 public final class ClearRow {
@@ -6,12 +15,20 @@ public final class ClearRow {
     private final int[][] newMatrix;
     private final int scoreBonus;
 
+    /**
+     * Constructs a ClearRow object
+     *
+     * @param linesRemoved number of rows removed
+     * @param newMatrix updated board state after rows were cleared
+     * @param scoreBonus score awarded for the removal
+     */
     public ClearRow(int linesRemoved, int[][] newMatrix, int scoreBonus) {
         this.linesRemoved = linesRemoved;
         this.newMatrix = newMatrix;
         this.scoreBonus = scoreBonus;
     }
 
+    /**how many times were removed*/
     public int getLinesRemoved() {
         return linesRemoved;
     }
@@ -20,6 +37,7 @@ public final class ClearRow {
         return MatrixOperations.copy(newMatrix);
     }
 
+    /** score bonue earned for the cleared rows*/
     public int getScoreBonus() {
         return scoreBonus;
     }
