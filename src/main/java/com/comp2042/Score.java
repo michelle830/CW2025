656f148fe99505e35ab7e7754d06d740a19169c6
michelle <efymc14@nottingham.edu.my>
2025-11-23 18:01:29 +0800

    Refactored Score.java for COMP2042

--- src/main/java/com/comp2042/Score.java
+++ src/main/java/com/comp2042/Score.java
@@ -1,3 +1,15 @@
+/**
+ * Represents the player's score for the Tetris game.
+ *
+ * Stores score as a JavaFX IntegerProperty so the GUI can bind to it
+ * and automatically updates whenever the score changes.
+ *
+ * Refactored for COMP2042 to:
+ * - Improve documentation
+ * - Clarify intent and usage
+ * - Follow clean-code conventions
+ */
+
 package com.comp2042;
 
 import javafx.beans.property.IntegerProperty;
@@ -5,17 +17,29 @@ import javafx.beans.property.SimpleIntegerProperty;
 
 public final class Score {
 
+    /** JavaFX property storing the player's current score */
     private final IntegerProperty score = new SimpleIntegerProperty(0);
 
+    /**
+     * @return the score property, allowing UI elements to bind to it
+     */
     public IntegerProperty scoreProperty() {
         return score;
     }
 
-    public void add(int i){
-        score.setValue(score.getValue() + i);
+    /**
+     * Adds the given amount to the player's current score.
+     *
+     * @param amount value to add
+     */
+    public void add(int amount){
+        score.set(score.get() + amount);
     }
 
+    /**
+     * Resets the score back to zero.
+     */
     public void reset() {
-        score.setValue(0);
+        score.set(0);
     }
 }
